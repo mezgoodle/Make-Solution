c2c48736a7e6a826f2ddf32c8ea0d8411e6b8dc5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-15 12:09:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -122,11 +122,11 @@
                                     <?php 
                                         $labels = ['Головна', 'Задачі', 'Корисні ресурси', 'Мови',
                                                 'Олімпіади', 'Правила сайту', 'Зворотній зв`язок', 'Книги для навчання']
-                                        $links = ["#home", "#about", "#features", "#screenshot", "#pricing",
+                                        $links_ = ["#home", "#about", "#features", "#screenshot", "#pricing",
                                                 "#team", "#contact", "books.php"]
                                         for ($i=0; $i < count($labels); $i++) { 
                                     ?>
-                                            <li class="nav-item active"><a class="nav-link" href="<?php echo $links[$i]; ?>"><?php echo $labels[$i]; ?></a></li>
+                                            <li class="nav-item active"><a class="nav-link" href="<?php echo $links_[$i]; ?>"><?php echo $labels[$i]; ?></a></li>
                                        <?php } ?>
                                 </ul>
                                 <div class="sing-up-button d-lg-none">
